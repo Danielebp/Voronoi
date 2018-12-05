f621d3b092b54f630cd34145cb46a5f982cc2a68
Danielebp <daniele.pecanha@gmail.com>
2018-12-05 15:52:45 -0800

    Including voronoi class for mpi

---
+++ ParallelCode/mpi/Voronoi.java
@@ -0,0 +1,14 @@
+package mpi;
+
+public class Voronoi {
+
+	public Voronoi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
