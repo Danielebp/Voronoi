c3fa876b84976212f319ff734b0c95987e5ea4a6
Jonas Pfab <jonas.pfab@tum.de>
2018-12-07 13:28:22 -0800

    Fix polygon+line intersection methods

--- Shared/sharedClasses/Polygon.java
+++ Shared/sharedClasses/Polygon.java
@@ -40,85 +40,57 @@ public class Polygon implements java.io.Serializable{
 
     // updates the current polygon with a better one if possible
     public void splitPolygon(Line line, Point initialPoint) {
-    	// 2 new polygons when splitting original with line
-    	Polygon p1 = new Polygon();
-    	Polygon p2 = new Polygon();
-
-    	// points of intersection of line with polygon
-    	// lines would either not intersect the polygon or intersect it exactly twice
-    	Point intersection1 = null;
-    	Point intersection2 = null;
-
-    	// first point pairs with the last
-    	Point last = points.get(points.size() - 1);;
-
-    	// iterates over all points that form the polygon
-        for (Point p : points) {
-		if(p.equals(initialPoint)) continue;
-    		// if we did not find any intersection or if we have seen both then we are on the first polygon
-    		if(intersection1 == null || intersection2 != null) {
-        		// adds last point to current polygon
-    			p1.addPoint(last);
-
-    			// check for intersection between current side and the line
-    			// current side is the line between last and current point
-	        	Point intersection = line.findIntersectionWithSide(last, p);
-	        	if(intersection != null) {
-	        		// if intersection was found add the intersection to both polygons
-	        		p1.addPoint(intersection);
-	        		p2.addPoint(intersection);
-
-	        		// saves intersection
-	        		intersection1 = intersection;
-	        	}
-    		} // if we have seen one of the intersections but have not seen the second yet then we are navigating the second polygon
-    		else {
-    			// adds last point to current polygon
-    			p2.addPoint(last);
-
-    			// check for intersection between current side and the line
-    			// current side is the line between last and current point
-    			Point intersection = line.findIntersectionWithSide(last, p);
-	        	if(intersection != null) {
-	        		// if intersection was found add the intersection to both polygons
-	        		p1.addPoint(intersection);
-	        		p2.addPoint(intersection);
-
-	        		// saves intersection
-	        		intersection2 = intersection;
-
-	        		// calculates in which side of the line the initial point is
-	        		double value = (intersection2.getX() - intersection1.getX())*(initialPoint.getY() - intersection1.getY()) -
-	        						(initialPoint.getX() - intersection1.getX())*(intersection2.getY() - intersection1.getY());
-	        		// initial point is in p1
-    				if (value > 0) {
-    					p2 = null;
-    				}
-    				else {// initial point in p2
-    					p1 = null;
-    					break; // if point is in p2 we dont need to find the rest of p1
-		        	}
-        		}
-
-        	}
-
-        	// updates last point with current
-        	last = p;
+        if (initialPoint.getX() == 855.0 && initialPoint.getY() == 595.0) {
+            System.out.println("Splitting polygon with line: " + line.toString());
         }
 
-    	if(p2 != null) {
-    		// polygon was only touched in one vertex so we just keep the original polygon
-    		if(p2.points.size() <= 2) return;
+        // 2 new polygons when splitting original with line
+        Polygon p1 = new Polygon();
+        Polygon p2 = new Polygon();
 
-    		// point was in p2, we have to update the current polygon
-    		this.points = p2.points;
-    	}
-    	// if point is in p1 then we check if any intersection was even found
-    	// if not we do not have to update anything
-    	else if (intersection1 != null){
-    		// point is in p1, we have to update the current polygon
-    		this.points = p1.points;
-    	}
+        // points of intersection of line with polygon
+        // lines would either not intersect the polygon or intersect it exactly twice
+        Point intersection1 = null;
+        Point intersection2 = null;
+
+        // first point pairs with the last
+        Point last = points.get(points.size() - 1);;
+
+        for (Point point : points) {
+            if (point.equals(initialPoint)) continue;
+            if (intersection1 == null || intersection2 != null) {
+                p1.addPoint(last);
+            } else {
+                p2.addPoint(last);
+            }
+
+            Point intersection = line.findIntersectionWithSide(last, point);
+            if (intersection != null) {
+                p1.addPoint(intersection);
+                p2.addPoint(intersection);
+                if (intersection1 == null) {
+                    intersection1 = intersection;
+                } else if (intersection2 == null) {
+                    intersection2 = intersection;
+                } else {
+                    System.err.println("Found three intersections");
+                }
+            }
+
+            last = point;
+        }
+
+        if (intersection1 != null && intersection2 != null) {
+            // calculates in which side of the line the initial point is
+            double value = (intersection2.getX() - intersection1.getX())*(initialPoint.getY() - intersection1.getY()) -
+                            (initialPoint.getX() - intersection1.getX())*(intersection2.getY() - intersection1.getY());
+            // initial point is in p1
+            if (value > 0) {
+                points = p1.points;
+            } else {// initial point in p2
+                points = p2.points;
+            }
+        }
     }
 
     @Override
